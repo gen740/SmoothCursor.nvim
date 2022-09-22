908524ef6afbbe8ac7de8aae6e301bf9260a0124
gen740 <genfujimoto@gmail.com>
2022-09-22 21:53:52 +0900

    chage print to vim.notify

--- plugin/smoothcursor.lua
+++ plugin/smoothcursor.lua
@@ -4,19 +4,25 @@ vim.api.nvim_create_user_command(
   require('smoothcursor.utils').smoothcursor_start,
   {}
 )
+
 vim.api.nvim_create_user_command(
   'SmoothCursorStop',
   require('smoothcursor.utils').smoothcursor_stop,
   {}
 )
+
 vim.api.nvim_create_user_command(
   'SmoothCursorToggle',
   require('smoothcursor.utils').smoothcursor_toggle,
   {}
 )
+
 vim.api.nvim_create_user_command('SmoothCursorStatus', function()
-  print(string.format('Status: %s', tostring(require('smoothcursor.utils').smoothcursor_status())))
+  vim.notify(
+    string.format('Status: %s', tostring(require('smoothcursor.utils').smoothcursor_status()))
+  )
 end, {})
+
 vim.api.nvim_create_user_command(
   'SmoothCursorDeleteSigns',
   require('smoothcursor.utils').smoothcursor_delete_signs,
